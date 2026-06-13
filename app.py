12bb5e4e931f1260c0dcf4231e1010145b547b39
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-06-13 19:02:44 +0530

    add json storage

--- app.py
+++ app.py
@@ -4,10 +4,18 @@ import string
 import random
 import qrcode
 import base64
+import os
+import json
 from io import BytesIO
 
+
 app = FastAPI()
-url = {}
+
+try:
+    with open("data.json", "r") as file:
+        url = json.load(file)
+except:
+    url = {}
 
 @app.get("/", response_class=HTMLResponse)
 def home():
@@ -63,33 +71,46 @@ def home():
     </html>
     """
 
+
 @app.get("/generate", response_class=HTMLResponse)
 def shortner(long_url: str):
+
+    if "." not in long_url:
+        long_url += ".com"
+    if not long_url.startswith(("http://", "https://")):
+        long_url = "https://" + long_url
     code = ''.join(
         random.choices(
             string.ascii_letters + string.digits,
-            k=6
+            k=7
         )
-    )
+   )
 
     url[code] = long_url
-    short_url = f"http://127.0.0.1:8000/{code}"
+
+    with open("data.json", "w") as file:
+        json.dump(url, file, indent=4)
+
+    BASE_URL = "http://127.0.0.1:8000"
+    short_url = f"{BASE_URL}/{code}"
+
     qr = qrcode.make(short_url)
 
     buffer = BytesIO()
     qr.save(buffer, format="PNG")
 
     img_base64 = base64.b64encode(buffer.getvalue()).decode()
+
     return f"""
     <html>
    
-<body style="font-family:Arial;text-align:center;margin-top:100px;">
- <h3>QR Code</h3>
+    <body style="font-family:Arial;text-align:center;margin-top:100px;">
+    <h3>QR Code</h3>
 
-<img
+    <img
     src="data:image/png;base64,{img_base64}"
     width="200"
->
+    >
     <h1>URL Generated Successfully!</h1>
 
     <p><b>Original URL:</b></p>
@@ -105,17 +126,9 @@ def shortner(long_url: str):
     <a href="/">
         <button>Generate Another</button>
     </a>
-<input
-    type="text"
-    value="{short_url}"
-    id="shortUrl"
-    readonly
-    style="width:350px;padding:10px;"
->
+    <input type="text" value="{short_url}" id="shortUrl" readonly style= "width:350px; padding:10px;">
 
-  <button onclick="copyUrl()">
-    Copy
-</button>
+    <button onclick="copyUrl()"> Copy </button>
 
 <script>
 function copyUrl() {{
